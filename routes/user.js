1bc1f0dcfd9d6bb2be35fa78c61e481cd17b79a4
Breno Henrique <brenosc2@hotmail.com>
2016-09-30 23:57:42 -0300

    minor fixes

--- routes/user.js
+++ routes/user.js
@@ -7,60 +7,60 @@ var csrfProtection = csrf();
 router.use(csrfProtection);
 
 router.get('/profile', isLoggedIn, function(req, res, next) {
-  res.render('user/profile');
+    res.render('user/profile');
 });
 
 router.get('/logout', isLoggedIn, function(req, res, next) {
-  req.logout();
-  res.redirect('/');
+    req.logout();
+    res.redirect('/');
 });
 
-router.use('/', notLoggedIn, function(req, res, next){
-  next();
+router.use('/', notLoggedIn, function(req, res, next) {
+    next();
 });
 
 router.get('/signup', function(req, res, next) {
-  var messages = req.flash('error');
-  res.render('user/signup', {
-    csrfToken: req.csrfToken(),
-    messages: messages,
-    hasErrors: messages.length > 0
-  });
+    var messages = req.flash('error');
+    res.render('user/signup', {
+        csrfToken: req.csrfToken(),
+        messages: messages,
+        hasErrors: messages.length > 0
+    });
 });
 
 router.post('/signup', passport.authenticate('local.signup', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signup',
-  failureFlash: true
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signup',
+    failureFlash: true
 }));
 
 router.get('/signin', function(req, res, next) {
-  var messages = req.flash('error');
-  res.render('user/signin', {
-    csrfToken: req.csrfToken(),
-    messages: messages,
-    hasErrors: messages.length > 0
-  });
+    var messages = req.flash('error');
+    res.render('user/signin', {
+        csrfToken: req.csrfToken(),
+        messages: messages,
+        hasErrors: messages.length > 0
+    });
 });
 
 router.post('/signin', passport.authenticate('local.signin', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signin',
-  failureFlash: true
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signin',
+    failureFlash: true
 }));
 
 module.exports = router;
 
 function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/');
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
 }
 
 function notLoggedIn(req, res, next) {
-  if (!req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/');
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
 }
