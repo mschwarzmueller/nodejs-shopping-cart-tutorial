cd8faa7dbf5177abdefd6a613dbe8cca4a201fcb
Breno Henrique <brenosc2@hotmail.com>
2016-12-12 18:34:10 -0200

    adicionando informações de usuário na rota /user/profile

--- models/user.js
+++ models/user.js
@@ -3,7 +3,10 @@ var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new Schema({
+    // name: {type: String, required: true},
     email: {type: String, required: true},
+    // city: {type: String, required: true},
+    // state: {type: String, required: true},
     password: {type: String, required: true}
 });
 
--- routes/user.js
+++ routes/user.js
@@ -19,7 +19,7 @@ router.get('/profile', isLoggedIn, function (req, res, next) {
             cart = new Cart(order.cart);
             order.items = cart.generateArray();
         });
-        res.render('user/profile', { orders: orders });
+        res.render('user/profile', { orders: orders, user: req.user });
     });
 });
 
