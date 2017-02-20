05c99132ab3127e5c4cefa402611bc7306a4421c
Breno Henrique <brenosc2@hotmail.com>
2017-02-20 12:36:30 -0300

    adding email and passwd form-groups to updateProfile

--- config/passport.js
+++ config/passport.js
@@ -2,79 +2,91 @@ var passport = require('passport');
 var User = require('../models/user');
 var LocalStrategy = require('passport-local').Strategy;
 
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
-        done(err, user);
-    });
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
 });
 
 passport.use('local.signup', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-}, function (req, email, password, done) {
-    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
-    req.checkBody('password', 'Invalid password').notEmpty().isLength({min:4});
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, function(req, email, password, done) {
+  req.checkBody('email', 'Email inválido!').notEmpty().isEmail();
+  req.checkBody('password', 'Senha inválida!').notEmpty().isLength({
+    min: 4
+  });
 
-    var errors = req.validationErrors();
-    if (errors) {
-        var messages = [];
-        errors.forEach(function(error) {
-           messages.push(error.msg);
-        });
-        return done(null, false, req.flash('error', messages));
-    }
-    User.findOne({'email': email}, function (err, user) {
-        if (err) {
-            return done(err);
-        }
-        if (user) {
-            return done(null, false, {message: 'Email is already in use.'});
-        }
-        var newUser = new User();
-        newUser.name = req.body.name;
-        newUser.city = req.body.city;
-        newUser.state = req.body.state;
-        newUser.email = email;
-        newUser.password = newUser.encryptPassword(password);
-        newUser.save(function(err, result) {
-           if (err) {
-               return done(err);
-           }
-           return done(null, newUser);
-        });
+  var errors = req.validationErrors();
+  if (errors) {
+    var messages = [];
+    errors.forEach(function(error) {
+      messages.push(error.msg);
     });
+    return done(null, false, req.flash('error', messages));
+  }
+  User.findOne({
+    'email': email
+  }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (user) {
+      return done(null, false, {
+        message: 'Este email já está sendo usado.'
+      });
+    }
+    var newUser = new User();
+    newUser.name = req.body.name;
+    newUser.city = req.body.city;
+    newUser.state = req.body.state;
+    newUser.email = email;
+    newUser.password = newUser.encryptPassword(password);
+    newUser.save(function(err, result) {
+      if (err) {
+        return done(err);
+      }
+      return done(null, newUser);
+    });
+  });
 }));
 
 passport.use('local.signin', new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
 }, function(req, email, password, done) {
-    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
-    req.checkBody('password', 'Invalid password').notEmpty();
-    var errors = req.validationErrors();
-    if (errors) {
-        var messages = [];
-        errors.forEach(function(error) {
-            messages.push(error.msg);
-        });
-        return done(null, false, req.flash('error', messages));
-    }
-    User.findOne({'email': email}, function (err, user) {
-        if (err) {
-            return done(err);
-        }
-        if (!user) {
-            return done(null, false, {message: 'No user found.'});
-        }
-        if (!user.validPassword(password)) {
-            return done(null, false, {message: 'Wrong password.'});
-        }
-        return done(null, user);
+  req.checkBody('email', 'Email inválido!').notEmpty().isEmail();
+  req.checkBody('password', 'Senha inválida!').notEmpty();
+  var errors = req.validationErrors();
+  if (errors) {
+    var messages = [];
+    errors.forEach(function(error) {
+      messages.push(error.msg);
     });
+    return done(null, false, req.flash('error', messages));
+  }
+  User.findOne({
+    'email': email
+  }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false, {
+        message: 'Usuário não encontrado!.'
+      });
+    }
+    if (!user.validPassword(password)) {
+      return done(null, false, {
+        message: 'Senha errada!'
+      });
+    }
+    return done(null, user);
+  });
 }));
--- routes/user.js
+++ routes/user.js
@@ -9,77 +9,90 @@ var Cart = require('../models/cart');
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
-router.get('/profile', isLoggedIn, function (req, res, next) {
-    Order.find({user: req.user}, function(err, orders) {
-        if (err) {
-            return res.write('Error!');
-        }
-        var cart;
-        orders.forEach(function(order) {
-            cart = new Cart(order.cart);
-            order.items = cart.generateArray();
-        });
-        res.render('user/profile', { orders: orders, user: req.user });
+router.get('/profile', isLoggedIn, function(req, res, next) {
+  Order.find({
+    user: req.user
+  }, function(err, orders) {
+    if (err) {
+      return res.write('Error!');
+    }
+    var cart;
+    orders.forEach(function(order) {
+      cart = new Cart(order.cart);
+      order.items = cart.generateArray();
     });
+    res.render('user/profile', {
+      orders: orders,
+      user: req.user
+    });
+  });
 });
 
-router.get('/logout', isLoggedIn, function (req, res, next) {
-    req.logout();
-    res.redirect('/');
+router.get('/logout', isLoggedIn, function(req, res, next) {
+  req.logout();
+  res.redirect('/');
 });
 
-router.use('/', notLoggedIn, function (req, res, next) {
-    next();
-});
-
-router.get('/signup', function (req, res, next) {
-    var messages = req.flash('error');
-    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+router.get('/signup', function(req, res, next) {
+  var messages = req.flash('error');
+  res.render('user/signup', {
+    csrfToken: req.csrfToken(),
+    messages: messages,
+    hasErrors: messages.length > 0
+  });
 });
 
 router.post('/signup', passport.authenticate('local.signup', {
-    failureRedirect: '/user/signup',
-    failureFlash: true
-}), function (req, res, next) {
-    if (req.session.oldUrl) {
-        var oldUrl = req.session.oldUrl;
-        req.session.oldUrl = null;
-        res.redirect(oldUrl);
-    } else {
-        res.redirect('/user/profile');
-    }
+  failureRedirect: '/user/signup',
+  failureFlash: true
+}), function(req, res, next) {
+  if (req.session.oldUrl) {
+    var oldUrl = req.session.oldUrl;
+    req.session.oldUrl = null;
+    res.redirect(oldUrl);
+  } else {
+    res.redirect('/user/profile');
+  }
 });
 
-router.get('/signin', function (req, res, next) {
-    var messages = req.flash('error');
-    res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+router.get('/signin', function(req, res, next) {
+  var messages = req.flash('error');
+  res.render('user/signin', {
+    csrfToken: req.csrfToken(),
+    messages: messages,
+    hasErrors: messages.length > 0
+  });
 });
 
 router.post('/signin', passport.authenticate('local.signin', {
-    failureRedirect: '/user/signin',
-    failureFlash: true
-}), function (req, res, next) {
-    if (req.session.oldUrl) {
-        var oldUrl = req.session.oldUrl;
-        req.session.oldUrl = null;
-        res.redirect(oldUrl);
-    } else {
-        res.redirect('/user/profile');
-    }
+  failureRedirect: '/user/signin',
+  failureFlash: true
+}), function(req, res, next) {
+  if (req.session.oldUrl) {
+    var oldUrl = req.session.oldUrl;
+    req.session.oldUrl = null;
+    res.redirect(oldUrl);
+  } else {
+    res.redirect('/user/profile');
+  }
+});
+
+router.use('/', notLoggedIn, function(req, res, next) {
+  next();
 });
 
 module.exports = router;
 
 function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/');
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
 }
 
 function notLoggedIn(req, res, next) {
-    if (!req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/');
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
 }
