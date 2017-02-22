3372d28e674b7448428ae4f6f581abe539b0710d
Breno Henrique <brenosc2@hotmail.com>
2017-02-22 15:51:22 -0300

    adding favicon

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ require('./config/passport');
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
