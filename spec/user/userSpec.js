76e633e3e68d7b1dd90273bb560c7e3e80b35cc5
Breno Henrique <brenosc2@hotmail.com>
2017-03-21 19:35:47 -0300

    refatorando testes do mongoose para userSpec.js

--- spec/user/userSpec.js
+++ spec/user/userSpec.js
@@ -1,8 +1,12 @@
+const dbURI = 'mongodb://localhost:27017/shopping';
+const should = require('chai').should();
+const mongoose = require('mongoose');
+
 const User = require('../../models/user');
-let user;
 
 describe('User Suite', () => {
 
+  let user;
   beforeEach(() => {
     user = new User();
     user.password = 'senhaTeste';
@@ -11,5 +15,41 @@ describe('User Suite', () => {
   it('should encrypt password', () => {
     expect(user.encryptPassword(user.password)).toContain('$2a$05');
   });
-  
+
+});
+
+describe("Deve testar o Usuario no banco de dados: ", function() {
+
+  beforeEach(function(done) {
+    if (mongoose.connection.db) return done();
+    mongoose.connect(dbURI, done);
+  });
+
+  it("está removendo!", function(done) {
+    User.find({
+      email: 'brenosc2@hotmail.com'
+    }).remove().exec();
+    return done();
+  });
+
+  it("está salvando!", function(done) {
+    const mockedUser = {
+      name: 'Breno Henrique',
+      city: 'Itaguara',
+      state: 'Minas Gerais',
+      email: 'brenosc2@hotmail.com',
+      password: 'senhaTeste'
+    }
+
+    const newUser = new User(mockedUser);
+    newUser.password = newUser.encryptPassword('senhaTeste');
+
+    newUser.save(function(err, result) {
+      if (err) {
+        return done(err);
+      }
+      return done(null, newUser);
+    });
+  });
+
 });
