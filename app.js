f343539649591959203f4dfa2955e7268595344c
Breno Henrique <brenosc2@hotmail.com>
2019-10-10 18:45:07 -0300

    indentando arquivos com 2 espaços

--- app.js
+++ app.js
@@ -1,86 +1,87 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var expressHbs = require('express-handlebars');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var passport = require('passport');
-var flash = require('connect-flash');
-var validator = require('express-validator');
-var MongoStore = require('connect-mongo')(session);
+var express = require("express")
+var path = require("path")
+var favicon = require("serve-favicon")
+var logger = require("morgan")
+var cookieParser = require("cookie-parser")
+var bodyParser = require("body-parser")
+var expressHbs = require("express-handlebars")
+var mongoose = require("mongoose")
+var session = require("express-session")
+var passport = require("passport")
+var flash = require("connect-flash")
+var validator = require("express-validator")
+var MongoStore = require("connect-mongo")(session)
 
-var routes = require('./routes/index');
-var userRoutes = require('./routes/user');
+var routes = require("./routes/index")
+var userRoutes = require("./routes/user")
 
-var app = express();
+var app = express()
 
-mongoose.connect(process.env.MONGO_DB_URI);
-require('./config/passport');
+mongoose.connect(process.env.MONGO_DB_URI)
+require("./config/passport")
 
 // view engine setup
-app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine(".hbs", expressHbs({ defaultLayout: "layout", extname: ".hbs" }))
+app.set("view engine", ".hbs")
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(validator());
-app.use(cookieParser());
-app.use(session({
-  secret: 'mysupersecret', 
-  resave: false, 
-  saveUninitialized: false,
-  store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  cookie: { maxAge: 180 * 60 * 1000 }
-}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")))
+app.use(logger("dev"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(validator())
+app.use(cookieParser())
+app.use(
+  session({
+    secret: "mysupersecret",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 180 * 60 * 1000 }
+  })
+)
+app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use(function(req, res, next) {
-    res.locals.login = req.isAuthenticated();
-    res.locals.session = req.session;
-    next();
-});
+  res.locals.login = req.isAuthenticated()
+  res.locals.session = req.session
+  next()
+})
 
-app.use('/user', userRoutes);
-app.use('/', routes);
+app.use("/user", userRoutes)
+app.use("/", routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error("Not Found")
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
+    res.status(err.status || 500)
+    res.render("error", {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
+  res.status(err.status || 500)
+  res.render("error", {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-
-module.exports = app;
+module.exports = app
