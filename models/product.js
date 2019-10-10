76a8e24d79096cd28d554d57b274a26ba24e7212
Breno Henrique <brenosc2@hotmail.com>
2019-10-10 18:44:39 -0300

    indentando models

--- models/product.js
+++ models/product.js
@@ -1,11 +1,11 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose = require("mongoose")
+var Schema = mongoose.Schema
 
 var schema = new Schema({
-    imagePath: {type: String, required: true},
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true}
-});
+  imagePath: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true }
+})
 
-module.exports = mongoose.model('Product', schema);
+module.exports = mongoose.model("Product", schema)
